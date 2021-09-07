a5d77fe117d7d6d8e483be0a87cfe9b81e9f4d6d
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-09-07 10:32:30 +0200

    logic for adding of decimal numbers

--- js/DecCalculator.js
+++ js/DecCalculator.js
@@ -5,14 +5,38 @@ class DecCalculator extends Calculator {
         let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         for (let i = numberX.length - 1; i >= 0; i--) {
           let carryBit = numberX[i] + numberY[i] + result[i];
-          if (carryBit === 2) {
+          if (carryBit === 10) {
             result[i] = 0;
             result[i - 1] = 1;
-          } else if (carryBit === 3) {
+          } else if (carryBit === 11) {
             result[i] = 1;
             result[i - 1] = 1;
-          } else {
-            result[i] = carryBit;
+          }   else if (carryBit === 12) {
+            result[i] = 2;
+            result[i - 1] = 1;
+          } else if (carryBit === 13) {
+            result[i] = 3;
+            result[i - 1] = 1;
+          } else if (carryBit === 14) {
+            result[i] = 4;
+            result[i - 1] = 1;
+          } else if (carryBit === 15) {
+            result[i] = 5;
+            result[i - 1] = 1;
+          } else if (carryBit === 16) {
+            result[i] = 6;
+            result[i - 1] = 1;
+          } else if (carryBit === 17) {
+            result[i] = 7;
+            result[i - 1] = 1;
+          }  else if (carryBit === 18) {
+            result[i] = 8;
+            result[i - 1] = 1;
+          } else if (carryBit === 19) {
+            result[i] = 9;
+            result[i - 1] = 1;
+          }else {
+            result[i] = carryBit
           }
         }
         return result;
