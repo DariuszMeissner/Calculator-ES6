420b7be8f3818717b355c9da4de5dd7dd7989309
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-09-07 09:56:42 +0200

    adding decimal number for method changeNumber

--- js/DecCalculator.js
+++ js/DecCalculator.js
@@ -1,7 +1,30 @@
 import Calculator from "./Calculator";
 
 class DecCalculator extends Calculator {
+    add(numberX, numberY) {
+        let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        for (let i = numberX.length - 1; i >= 0; i--) {
+          let carryBit = numberX[i] + numberY[i] + result[i];
+          if (carryBit === 2) {
+            result[i] = 0;
+            result[i - 1] = 1;
+          } else if (carryBit === 3) {
+            result[i] = 1;
+            result[i - 1] = 1;
+          } else {
+            result[i] = carryBit;
+          }
+        }
+        return result;
+      }
 
+      changeNumber(root) {
+        const input = root.firstElementChild;
+        input.innerText = +input.innerText < 9 ? ++input.innerText : 0;
+    
+        this.checkNumber();
+        this.updateResult();
+      }
 }
 
 export default DecCalculator;
